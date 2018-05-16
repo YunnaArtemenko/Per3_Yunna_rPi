e673715572d19753a095044482e487c9392a5f95
YunnaArtemenko <yunna.artemenko@gmail.com>
2018-05-16 18:20:23 +0000

    turns on LED when button is  pressed down on

--- gpio_control.py
+++ gpio_control.py
@@ -4,7 +4,6 @@ from time import sleep
 led = LED(19)
 button = Button(16)
 
-while True:
-    button.wait_for_press()
-    led.toggle()
-    sleep(0.5)
+button.when_pressed = led.on
+button.when_released = led.off
+pause()
